fbeb3c977e27e81a1deb980c83155d26578d4162
chzellweger <christian.zellweger@derbund.ch>
2019-02-12 13:33:50 +0100

    adjust markup

--- routes/routes.js
+++ routes/routes.js
@@ -10,11 +10,18 @@ router.get('/machine/:action/:vote', async (req, res) => {
   try {
     const publicUrl = await handleCharts(req.params)
 
-    res.json({
-      status: "ok",
-      publicUrl,
-      payload: req.params,
-    })
+    const response = {
+      "status": "ok",
+      payload: req.params
+    }
+
+    if (req.params.action === 'start') {
+      res.json(Object.assign(response, {publicUrl}))
+    } else if (req.params.action === 'stop') {
+      res.json(response)
+    } else {
+      throw new Error(`action unknown: ${rep.params.action}`)
+    }
     res.end()
   } catch(error) {
     res.json({status: "failed", message: error})
