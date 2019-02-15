08626cf807febd104a33dbdad65ab176115db5e5
chzellweger <christian.zellweger@derbund.ch>
2019-02-15 21:35:07 +0100

    rename files

--- controllers/data/handleData.js
+++ controllers/data/handleData.js
@@ -1,12 +1,12 @@
-const dataHandler = require('../../services/data/dataHandler')
+const dataHandlers = require('../../services/data/dataHandlers')
 
 async function handleData(voteId) {
   try {
-    const data = await dataHandler.getData(voteId)
+    const data = await dataHandlers.getData(voteId)
 
-    let output = dataHandler.shapeData(data)
+    let output = dataHandlers.shapeData(data)
 
-    output = dataHandler.handleSpecialCases(output, data)
+    output = dataHandlers.handleSpecialCases(output, data)
     console.log(output)
 
     return output
